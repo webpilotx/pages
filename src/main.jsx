2a03a3fd6eb41964a16fee005a1f304ee746b93a
chientrm <chientrm@gmail.com>
2025-04-05 13:47:40 +0700

    Revert "Refactor data fetching in PagesList and CreatePage components to use a unified fetchData helper function for improved error handling and code clarity"
    This reverts commit 2e71f0abeee35e253749648bfc7f1e0e1804392d.

--- src/main.jsx
+++ src/main.jsx
@@ -12,27 +12,31 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
-// Helper function to fetch data
-const fetchData = async (url, errorMessage) => {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(errorMessage);
-    return await response.json();
-  } catch (error) {
-    console.error(errorMessage, error);
-    return null;
-  }
-};
-
 function PagesList() {
   const [pagesList, setPagesList] = useState([]);
   const navigate = useNavigate();
 
+  const fetchPagesList = async () => {
+    try {
+      const response = await fetch("/pages/api/pages-list");
+      const data = await response.json();
+      setPagesList(data);
+    } catch (error) {
+      console.error("Error fetching pages list:", error);
+    }
+  };
+
   useEffect(() => {
-    fetchData("/pages/api/pages-list", "Error fetching pages list").then(
-      (data) => setPagesList(data || [])
-    );
+    fetchPagesList();
   }, []);
+
+  const handlePageClick = (page) => {
+    navigate(`/pages/${page.id}/edit`); // Navigate to /pages/:id/edit by default
+  };
+
+  const handleCreatePage = () => {
+    navigate("/pages/new");
+  };
 
   return (
     <>
@@ -42,7 +46,7 @@ function PagesList() {
           <li
             key={page.id}
             className="p-4 bg-gray-100 rounded-md shadow-sm border border-gray-300 cursor-pointer hover:bg-gray-200"
-            onClick={() => navigate(`/pages/${page.id}/edit`)}
+            onClick={() => handlePageClick(page)}
           >
             <p>
               <strong>Repo:</strong> {page.repo}
@@ -57,7 +61,7 @@ function PagesList() {
         ))}
       </ul>
       <button
-        onClick={() => navigate("/pages/new")}
+        onClick={handleCreatePage}
         className="mt-8 px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
       >
         Create
@@ -84,23 +88,85 @@ function CreatePage() {
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchData("/pages/api/provider-accounts", "Error fetching accounts").then(
-      (data) => {
-        setAccounts(data || []);
-        if (data?.length > 0) setSelectedAccount(data[0].login);
+  const fetchAccounts = async () => {
+    try {
+      const response = await fetch("/pages/api/provider-accounts");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching accounts:", error);
+      return [];
+    }
+  };
+
+  const fetchRepositories = async (accountLogin) => {
+    try {
+      const response = await fetch(
+        `/pages/api/repositories?account=${accountLogin}`
+      );
+      const data = await response.json();
+      return data.repositories;
+    } catch (error) {
+      console.error("Error fetching repositories:", error);
+      return [];
+    }
+  };
+
+  const fetchBranches = async (repoFullName) => {
+    try {
+      const response = await fetch(`/pages/api/branches?repo=${repoFullName}`);
+      const data = await response.json();
+      return data.branches;
+    } catch (error) {
+      console.error("Error fetching branches:", error);
+      return [];
+    }
+  };
+
+  const handleCreateNewPage = async (newPage) => {
+    try {
+      const response = await fetch("/pages/api/create-page", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newPage),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create page");
       }
-    );
+
+      navigate("/pages");
+    } catch (error) {
+      console.error("Error creating page:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAccounts()
+      .then((fetchedAccounts) => {
+        setAccounts(fetchedAccounts || []);
+        if (fetchedAccounts?.length > 0) {
+          setSelectedAccount(fetchedAccounts[0].login);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching accounts:", error);
+        setAccounts([]);
+      });
   }, []);
 
   useEffect(() => {
     if (selectedAccount) {
       setLoadingRepos(true);
-      fetchData(
-        `/pages/api/repositories?account=${selectedAccount}`,
-        "Error fetching repositories"
-      )
-        .then((data) => setRepositories(data?.repositories || []))
+      fetchRepositories(selectedAccount)
+        .then((repos) => {
+          setRepositories(repos || []);
+          setCurrentPage(1);
+        })
+        .catch((error) => {
+          console.error("Error fetching repositories:", error);
+          setRepositories([]);
+        })
         .finally(() => setLoadingRepos(false));
     }
   }, [selectedAccount]);
@@ -108,11 +174,42 @@ function CreatePage() {
   useEffect(() => {
     if (repo) {
       setLoadingBranches(true);
-      fetchData(`/pages/api/branches?repo=${repo}`, "Error fetching branches")
-        .then((data) => setBranches(data?.branches || []))
+      fetchBranches(repo)
+        .then((fetchedBranches) => {
+          setBranches(fetchedBranches || []);
+          if (fetchedBranches?.length > 0) {
+            setBranch(fetchedBranches[0]);
+          } else {
+            setBranch("");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching branches:", error);
+          setBranches([]);
+          setBranch("");
+        })
         .finally(() => setLoadingBranches(false));
     }
   }, [repo]);
+
+  const handleAccountChange = (accountLogin) => {
+    setSelectedAccount(accountLogin);
+  };
+
+  const handleRepoSelect = (selectedRepo) => {
+    if (repo !== selectedRepo) {
+      setRepo(selectedRepo);
+    }
+  };
+
+  const handleAddEnvVar = () => {
+    setEnvVars([...envVars, { name: "", value: "" }]);
+  };
+
+  const handleRemoveEnvVar = (index) => {
+    const updatedEnvVars = envVars.filter((_, i) => i !== index);
+    setEnvVars(updatedEnvVars);
+  };
 
   const handleEnvVarChange = (index, field, value) => {
     const updatedEnvVars = [...envVars];
@@ -122,43 +219,64 @@ function CreatePage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const newPage = { repo, name, branch, buildScript, envVars };
-    const response = await fetch("/pages/api/create-page", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newPage),
-    });
-    if (response.ok) {
+    try {
+      const response = await fetch("/pages/api/create-page", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ repo, name, branch, buildScript, envVars }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create page");
+      }
+
       const { pageId, deploymentId } = await response.json();
+
+      // Navigate to the logs page for the new deployment
       navigate(`/pages/${pageId}/logs/${deploymentId}`);
-    } else {
-      console.error("Failed to create page");
+    } catch (error) {
+      console.error("Error creating page:", error);
     }
   };
 
-  const currentRepos = repositories.slice(
-    (currentPage - 1) * reposPerPage,
-    currentPage * reposPerPage
-  );
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
+  const currentRepos = repositories.slice(indexOfFirstRepo, indexOfLastRepo);
+
+  const totalPages = Math.ceil(repositories.length / reposPerPage);
 
   return (
     <div className="mt-8 p-6 bg-gray-100 rounded-md shadow-lg">
-      {step === 1 ? (
+      {step === 1 && (
         <>
           <h2 className="text-2xl font-bold mb-4">Choose Repository</h2>
           <div>
             <label className="block mb-2">Provider Account</label>
             <select
               value={selectedAccount}
-              onChange={(e) => setSelectedAccount(e.target.value)}
+              onChange={(e) => handleAccountChange(e.target.value)}
               className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
+              required
             >
+              <option value="" disabled>
+                Select an account
+              </option>
               {accounts.map((account) => (
                 <option key={account.login} value={account.login}>
                   {account.login}
                 </option>
               ))}
             </select>
+            <a
+              href={`https://github.com/login/oauth/authorize?client_id=${
+                import.meta.env.VITE_GITHUB_CLIENT_ID
+              }&scope=repo`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block mt-2 text-blue-500 hover:underline"
+            >
+              Authorize more GitHub accounts
+            </a>
           </div>
           <div className="mt-6">
             <label className="block mb-2">Repository</label>
@@ -167,21 +285,41 @@ function CreatePage() {
                 Loading repositories...
               </p>
             ) : (
-              <div className="grid grid-cols-2 gap-4">
-                {currentRepos.map((repository) => (
-                  <div
-                    key={repository.full_name}
-                    className={`p-4 bg-gray-100 rounded-md shadow-sm border cursor-pointer ${
-                      repo === repository.full_name
-                        ? "border-blue-500"
-                        : "border-gray-300"
-                    }`}
-                    onClick={() => setRepo(repository.full_name)}
-                  >
-                    {repository.full_name}
-                  </div>
-                ))}
-              </div>
+              <>
+                <div className="grid grid-cols-2 gap-4">
+                  {currentRepos.map((repository) => (
+                    <div
+                      key={repository.full_name}
+                      className={`p-4 bg-gray-100 rounded-md shadow-sm border cursor-pointer ${
+                        repo === repository.full_name
+                          ? "border-blue-500"
+                          : "border-gray-300"
+                      }`}
+                      onClick={() => handleRepoSelect(repository.full_name)}
+                      title={repository.full_name}
+                    >
+                      <span className="block truncate">
+                        {repository.full_name}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+                <div className="mt-4 flex justify-center space-x-2">
+                  {Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                      key={index}
+                      onClick={() => setCurrentPage(index + 1)}
+                      className={`px-3 py-1 rounded-md ${
+                        currentPage === index + 1
+                          ? "bg-blue-500 text-white"
+                          : "bg-gray-300 text-black"
+                      }`}
+                    >
+                      {index + 1}
+                    </button>
+                  ))}
+                </div>
+              </>
             )}
           </div>
           <button
@@ -192,75 +330,116 @@ function CreatePage() {
             Begin Setup
           </button>
         </>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <h2 className="text-2xl font-bold mb-4">Setup Page Details</h2>
-          <div className="mb-4">
-            <label className="block mb-2">Page Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Branch</label>
-            <select
-              value={branch}
-              onChange={(e) => setBranch(e.target.value)}
-              className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
-              required
-            >
-              {branches.map((branch) => (
-                <option key={branch} value={branch}>
-                  {branch}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Build Script</label>
-            <textarea
-              value={buildScript}
-              onChange={(e) => setBuildScript(e.target.value)}
-              className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
-              rows="4"
-            ></textarea>
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Environment Variables</label>
-            {envVars.map((env, index) => (
-              <div key={index} className="flex space-x-4 mb-2">
+      )}
+      {step === 2 && (
+        <>
+          {loadingBranches ? (
+            <p className="text-center text-gray-500">Fetching branches...</p>
+          ) : branches.length > 0 ? (
+            <form onSubmit={handleSubmit}>
+              <h2 className="text-2xl font-bold mb-4">Setup Page Details</h2>
+              <div className="mb-4">
+                <label className="block mb-2">Page Name</label>
                 <input
                   type="text"
-                  placeholder="Name"
-                  value={env.name}
-                  onChange={(e) =>
-                    handleEnvVarChange(index, "name", e.target.value)
-                  }
-                  className="w-1/2 px-4 py-2 bg-gray-200 text-black rounded-md"
-                />
-                <input
-                  type="text"
-                  placeholder="Value"
-                  value={env.value}
-                  onChange={(e) =>
-                    handleEnvVarChange(index, "value", e.target.value)
-                  }
-                  className="w-1/2 px-4 py-2 bg-gray-200 text-black rounded-md"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
+                  required
                 />
               </div>
-            ))}
-          </div>
-          <button
-            type="submit"
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Save and Deploy
-          </button>
-        </form>
+              <div className="mb-4">
+                <label className="block mb-2">Branch</label>
+                <select
+                  value={branch}
+                  onChange={(e) => setBranch(e.target.value)}
+                  className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
+                  required
+                >
+                  <option value="" disabled>
+                    Select a branch
+                  </option>
+                  {branches.map((branch) => (
+                    <option key={branch} value={branch}>
+                      {branch}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2">Build Script (Optional)</label>
+                <textarea
+                  value={buildScript}
+                  onChange={(e) => setBuildScript(e.target.value)}
+                  placeholder={`pnpm install\npnpm run build\npnpm run migrate`}
+                  className="w-full px-4 py-2 bg-gray-200 text-black rounded-md"
+                  rows="4"
+                ></textarea>
+              </div>
+              <div className="mb-4">
+                <label className="block mb-2">Environment Variables</label>
+                {envVars.map((env, index) => (
+                  <div key={index} className="flex space-x-4 mb-2">
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      value={env.name}
+                      onChange={(e) =>
+                        handleEnvVarChange(index, "name", e.target.value)
+                      }
+                      className="w-1/2 px-4 py-2 bg-gray-200 text-black rounded-md"
+                      required
+                    />
+                    <input
+                      type="text"
+                      placeholder="Value"
+                      value={env.value}
+                      onChange={(e) =>
+                        handleEnvVarChange(index, "value", e.target.value)
+                      }
+                      className="w-1/2 px-4 py-2 bg-gray-200 text-black rounded-md"
+                      required
+                    />
+                    <button
+                      onClick={() => handleRemoveEnvVar(index)}
+                      type="button"
+                      className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                ))}
+                <button
+                  onClick={handleAddEnvVar}
+                  type="button"
+                  className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+                >
+                  Add Env
+                </button>
+              </div>
+              <div className="flex justify-between">
+                <button
+                  onClick={() => setStep(1)}
+                  type="button"
+                  className="px-4 py-2 bg-gray-300 text-black rounded-md hover:bg-gray-400"
+                >
+                  Back
+                </button>
+                <button
+                  type="submit"
+                  disabled={!name || !branch}
+                  className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 disabled:opacity-50"
+                >
+                  Save and Deploy
+                </button>
+              </div>
+            </form>
+          ) : (
+            <p className="text-center text-gray-500">
+              No branches available for the selected repository.
+            </p>
+          )}
+        </>
       )}
     </div>
   );
